fc9f8770e6dd37a4769acab8e975d3a8eb22bd3f
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-10-16 20:08:25 -0500

    added more styling

--- src/components/Rounds/Rounds.jsx
+++ src/components/Rounds/Rounds.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import styles from "./Rounds.css";
+import "./Rounds.css";
+import Button from "@mui/material/Button";
 
 //need to update this page and somehow add the course name to the rounds displayed
 
 export default function Rounds() {
   const dispatch = useDispatch();
+  const history = useHistory();
 
+  // Function to handle button click and navigate to the Add Rounds page
+  const goToAddRounds = () => {
+    history.push("/addRound");
+  };
   // Get the rounds from the Redux store
   const rounds = useSelector((state) => state.rounds);
 
@@ -42,18 +49,27 @@ export default function Rounds() {
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Rounds</h1>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={goToAddRounds}
+        className="addButton"
+      >
+        Add Round
+      </Button>
+
       {rounds.map((round) => (
-        <Card key={round.id} className={styles.styledCard}>
-          <CardContent className={styles.cardContent}>
-            <Typography variant="h5" className={styles.score}>
+        <Card key={round.id} className="styledCard">
+          <CardContent className="cardContent">
+            <Typography variant="h5" className="score">
               Score: {round.front_9_score + round.back_9_score}
             </Typography>
-            <Typography variant="subtitle1" className={styles.date}>
+            <Typography variant="subtitle1" className="date">
               {formatDate(round.date)}
             </Typography>
-            <Typography variant="subtitle2" className={styles.courseHandicap}>
+            <Typography variant="subtitle2" className="courseHandicap">
               Course Handicap: {round.course_handicap}
             </Typography>
           </CardContent>
