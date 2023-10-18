eee92d9338f6fc894fb2d250bc0f210ffb70d0cc
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-10-18 16:10:49 -0500

    deleted some unused code

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -6,20 +6,29 @@ function UserPage() {
   const dispatch = useDispatch();
 
   const user = useSelector((store) => store.user);
-
-  const rounds = useSelector((store) => store.rounds);
-  console.log("logging fucking rounds", rounds);
-  //TODO component to display rounds, import here, get total count and display here
+  useEffect(() => {
+    dispatch({ type: "FETCH_ROUNDS" });
+  }, []);
+  const round = useSelector((store) => store.rounds);
 
   // useEffect(() => {
   //   dispatch({ type: "FETCH_USER" });
   // }, []);
+
+  // Extract a list of course IDs from the rounds data
+  const courseIdsPlayed = round.map((round) => round.course_id);
+  // Convert this list into a set to get unique course IDs
+  const uniqueCoursesPlayed = new Set(courseIdsPlayed);
+  // The size of this set is the number of unique courses played
+  const numberOfUniqueCoursesPlayed = uniqueCoursesPlayed.size;
 
   return (
     <div className="userPageBackground">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
       <p>Handicap Index: {user.user_handicap}</p>
+      <p>Rounds Played: {round.length}</p>
+      <p>Courses Played: {numberOfUniqueCoursesPlayed}</p>
     </div>
   );
 }
