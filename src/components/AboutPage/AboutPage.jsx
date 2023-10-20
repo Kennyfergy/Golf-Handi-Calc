0022829ac2a6730a82473570d6f915c81b532336
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-10-20 15:20:08 -0500

    made an about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,16 +1,58 @@
-import React from 'react';
-
-// This is one of our simplest components
-// It doesn't have local state,
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is'
+import React from "react";
 
 function AboutPage() {
   return (
     <div className="container">
-      <div>
-        <p>This about page is for anyone to read!</p>
-      </div>
+      <h2>About the App</h2>
+
+      <h3>
+        How is the Handicap Index calculated using the World Handicap System?
+      </h3>
+      <p>
+        The WHS calculates the Handicap Index based on a sliding scale of a
+        player's lowest score differentials:
+      </p>
+      <ul>
+        <li>Best 3 rounds: Use 1 differential</li>
+        <li>Best 8 rounds: Use 2 differentials</li>
+        <li>Best 10 rounds: Use 3 differentials</li>
+        <li>Best 12 rounds: Use 4 differentials</li>
+        <li>Best 14 rounds: Use 5 differentials</li>
+        <li>Best 16 rounds: Use 6 differentials</li>
+        <li>Best 18 rounds: Use 7 differentials</li>
+        <li>Everything after: Use best 8 differentials</li>
+      </ul>
+      <p>
+        Depending on the number of rounds played, the appropriate number of
+        lowest differentials is taken into account to compute the average, which
+        then represents the Handicap Index.
+      </p>
+
+      <h3>How is the Course Handicap calculated using the WHS?</h3>
+      <p>
+        The Course Handicap represents the number of strokes a player receives
+        on a specific course. It's calculated by multiplying the Handicap Index
+        by the Slope Rating of the course (and dividing by 113, which is the
+        standard Slope Rating). Then, the resulting number is rounded to the
+        nearest whole number.
+      </p>
+      <p>Formula: Course Handicap = (Handicap Index × Slope Rating / 113)</p>
+      <p>
+        It ensures players of varying abilities can compete fairly on any golf
+        course around the world.
+      </p>
+      <p>
+        For a comprehensive understanding and more details on the World Handicap
+        System, visit the official{" "}
+        <a
+          href="https://www.whs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          WHS website
+        </a>
+        .
+      </p>
     </div>
   );
 }
